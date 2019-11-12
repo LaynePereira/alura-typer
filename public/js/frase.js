e93c9d1b3ae2aafdc3b652cf0332a758850c5e7b
Jéssika Layne <jessika.layne.pereira@gmail.com>
2019-11-12 17:42:05 -0300

    Enviando dados AJAX

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,12 +1,19 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria() {
+
+    $("#spinner").toggle();
+
     $.get("http://localhost:3000/frases", trocaFraseAleatoria)
     .fail(function(){
         $("#erro").toggle();
         setTimeout(function(){
             $("#erro").toggle();
         },1500);
+    })
+    .always(function(){ //sempre escondendo o spinner
+        $("#spinner").toggle();
     });
 }
 
@@ -15,5 +22,16 @@ function trocaFraseAleatoria(data) {
     var numeroAleatorio = Math.floor(Math.random() * data.length);
     frase.text(data[numeroAleatorio].texto);
     atualizaTamanhoFrase();
-    atualizaTempoInicial(data[numeroAleatorio].tempo);
+    atualizaTempoInicial(data[numeroAleatorio].tempo);  
+}
+
+function buscaFrase () {
+    var fraseId = $("#frase-id").val();
+    var dados = {id: fraseId};
+    $.get("http://localhost:3000/frases", dados,trocaFrase);
+
+}
+
+function trocaFrase(){
+    console.log("busquei!");
 }
