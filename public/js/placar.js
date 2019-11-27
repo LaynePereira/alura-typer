c22f8b2ff0f23e7246aaf1a10423371f804e2878
Jéssika Layne <jessika.layne.pereira@gmail.com>
2019-11-27 11:20:53 -0300

    modified placar

--- public/js/placar.js
+++ public/js/placar.js
@@ -85,7 +85,6 @@ function sincronizaPlacar() {
 function atualizaPlacar(){
     $.get("http://localhost:3000/placar",function(data){
         $(data).each(function(){
-
             var linha = novaLinha(this.usuario, this.pontos);
             
             linha.find(".botao-remover").click(removeLinha);
